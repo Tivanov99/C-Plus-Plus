62a92b28416513f9541d39760964bab66d3ccba6
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-12 20:10:50 +0300

    Update Puma.h constructor

--- OppTrainingC++/Puma.h
+++ OppTrainingC++/Puma.h
@@ -5,8 +5,10 @@ class Puma :
 	public Animal, public Carnivores
 {
 public:
-	Puma(string name, int age, double weight, int huntingLevel = 1, int levelOfDanger = 60, bool isVaccinated = false)
-		:Animal(name, age, weight, isVaccinated)
+	Puma(string name, int age, double weight, int huntingLevel = 1,
+		int levelOfDanger = 60, bool isVaccinated = false,
+		int happyLevel = 40, int foodLevel = 30)
+		:Animal(name, age, weight, happyLevel, foodLevel, isVaccinated)
 		, Carnivores(huntingLevel, levelOfDanger) {
 	};
 };
