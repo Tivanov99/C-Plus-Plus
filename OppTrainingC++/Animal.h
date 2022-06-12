3487f946341a3cc6c11636c7b522924da47ac03e
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-12 20:19:14 +0300

    Add play method to Animal.h

--- OppTrainingC++/Animal.h
+++ OppTrainingC++/Animal.h
@@ -5,7 +5,7 @@ using namespace std;
 
 class Animal {
 public:
-	Animal(string name, int age, double weight,int happyLevel ,int foodLevel,bool isVaccinated = false) {
+	Animal(string name, int age, double weight, int happyLevel, int foodLevel, bool isVaccinated = false) {
 		Name = name;
 		Age = age;
 		Vaccinated = isVaccinated;
@@ -29,6 +29,20 @@ public:
 	}
 	bool IsVaccinated() {
 		return Vaccinated;
+	}
+	virtual void GetAnimalType() = 0;
+	void Play() {
+		if (FoodLevel <= 10) {
+			cout<<Name << " need some food!";
+		}
+		else
+		{
+			FoodLevel -= 10;
+			if ((HappyLevel + 10) <= 100) {
+				HappyLevel += 10;
+			};
+			cout << Name << " is Playing! His Happy level is " << HappyLevel;
+		}
 	}
 	~Animal() {
 
