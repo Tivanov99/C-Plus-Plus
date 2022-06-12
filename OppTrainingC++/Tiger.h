32bf6ccf7b2040e8009e5a084b36dfce0ba09387
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-12 22:27:27 +0300

    Added Tiger.h

---
+++ OppTrainingC++/Tiger.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Animal.h"
+#include "Carnivores.h"
+class Tiger :
+    public Animal, public Carnivores
+{
+public:
+	Tiger(string name, int age, double weight, int huntingLevel = 2,
+		int levelOfDanger = 80, bool isVaccinated = false,
+		int happyLevel = 30, int foodLevel = 40)
+		:Animal(name, age, weight, happyLevel, foodLevel, isVaccinated)
+		, Carnivores(huntingLevel, levelOfDanger) {
+	};
+	void GetAnimalType() {
+		cout << Name << " Is " << "Tiger!\n";
+	}
+	void MakeSpecialSound() {
+		cout << "Roar\n";
+	}
+};
+
