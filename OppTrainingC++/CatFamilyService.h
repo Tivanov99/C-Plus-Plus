a77dd21e2664ea9def40bceddbe6e15f7f6693ba
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-14 23:13:45 +0300

    Fix

--- OppTrainingC++/CatFamilyService.h
+++ OppTrainingC++/CatFamilyService.h
@@ -18,47 +18,39 @@ public:
 			cout << "Animal Name : ";
 			string name;
 			cin >> name;
-			cout << "\n";
 
 			cout << "Animal Age: ";
 			int age;
 			cin >> age;
-			cout << "\n";
 
-			cout << "Animal Weight";
+			cout << "Animal Weight: ";
 			double weight;
 			cin >> weight;
-			cout << "\n";
 
 			cout << OptionalAnimalParameterMessage;
-			cout << "Animal HuntingLevel: " << Low << "Low" << Medium << "Medium"
-				 << High << "High" << Professional << "Professional" << ": ";
+			cout << "Animal HuntingLevel: " << Low << ".Low " << Medium << ".Medium "
+				 << High << ".High " << Professional << ".Professional " << ": ";
 			int hunttingLevel;
 			cin >> hunttingLevel;
-			cout << "\n";
 
 			cout << OptionalAnimalParameterMessage;
 			cout << "Animal Level of danger: ";
 			int LevelOfDanger;
 			cin >> LevelOfDanger;
-			cout << "\n";
 
 			cout << "Animal Vaccinated (true/false) : ";
 			bool Vaccinated;
 			cin >> Vaccinated;
-			cout << "\n";
 
 			cout << OptionalAnimalParameterMessage;
 			cout << "Animal Happy Level: ";
 			int HappyLevel;
 			cin >> HappyLevel;
-			cout << "\n";
 
 			cout << OptionalAnimalParameterMessage;
 			cout << "Animal Food Level: ";
 			int FoodLevel;
 			cin >> FoodLevel;
-			cout << "\n";
 
 		}
 		catch (const std::exception&)
