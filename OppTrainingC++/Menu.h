8ae80cd21cc213ff6dadd550e89eae94ff6c44ef
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-17 16:20:59 +0300

    Added while cycle in Menu.h MainMenu which will validate user start input

--- OppTrainingC++/Menu.h
+++ OppTrainingC++/Menu.h
@@ -12,8 +12,23 @@ public:
 	void MainMenu() {
 		cout << "Wellcome to Main menu!\n";
 		cout << "1.Go to user menu.\n";
-		cout << "2.Go to admin menu.\n";
-
+		cout << "2.Go to admin menu.\n\n";
+		while (true)
+		{
+			cout << "Enter you choice: ";
+			try
+			{
+				string userInput;
+				cin >> userInput;
+				int value = stoi(userInput);
+			}
+			catch (const std::exception&)
+			{
+				cout << "\nInvalid choice! Try again!\n";
+				continue;
+			}
+		}
+		
 	};
 	void AdminMenu() {
 		cout << "Wellcome to admin menu!\n";
