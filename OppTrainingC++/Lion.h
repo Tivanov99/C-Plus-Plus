52f5fed5a283ac7533c262eb00df479376ed8d12
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-20 20:19:28 +0300

    Lion.h and Tiger.h inherits CarnivoresAnimal

--- OppTrainingC++/Lion.h
+++ OppTrainingC++/Lion.h
@@ -1,22 +1,28 @@
 #pragma once
 #include "Animal.h"
-#include "Carnivores.h"
+#include "CarnivoresAnimal.h"
 
-class Lion : public Animal,
-	public Carnivores
+class Lion : public CarnivoresAnimal
 {
 public:
 	Lion(string name, int age, double weight, int huntingLevel = 1,
 		int levelOfDanger = 70, bool isVaccinated = false,
 		int happyLevel = 60, int foodLevel = 0)
-		:Animal(name, age, weight, happyLevel, foodLevel, isVaccinated)
-		, Carnivores(huntingLevel, levelOfDanger) {
+		:CarnivoresAnimal(name, age, weight, happyLevel, foodLevel, isVaccinated,
+			huntingLevel, levelOfDanger) {
 	};
+
 	void ShowAnimalType() {
 		cout << "The " << Name << " is " << "Lion";
 	}
 	void MakeSpecialSound() {
 		cout << "Roar\n";
+	}
+	void ShowFavouriteFood() {
+		cout << "Lion's main diet is ungulates such as deer, horses, elk, cattle and sheep.";
+	}
+	void ShowHuntingType() {
+		cout << "Lion's hunting type is the following: Waiting his booty in the bush and when she is distracted jumps and drives nails and teeth into it.";
 	}
 };
 
--- OppTrainingC++/Tiger.h
+++ OppTrainingC++/Tiger.h
@@ -1,21 +1,27 @@
 #pragma once
 #include "Animal.h"
-#include "Carnivores.h"
+#include "CarnivoresAnimal.h"
 class Tiger :
-    public Animal, public Carnivores
+    public CarnivoresAnimal
 {
 public:
 	Tiger(string name, int age, double weight, int huntingLevel = 2,
 		int levelOfDanger = 80, bool isVaccinated = false,
 		int happyLevel = 30, int foodLevel = 40)
-		:Animal(name, age, weight, happyLevel, foodLevel, isVaccinated)
-		, Carnivores(huntingLevel, levelOfDanger) {
+		:CarnivoresAnimal(name, age, weight, happyLevel, foodLevel, isVaccinated,
+		huntingLevel, levelOfDanger) {
 	};
 	void ShowAnimalType() {
 		cout << "The " << Name << " is " << "Tiger";
 	}
 	void MakeSpecialSound() {
 		cout << "Roar\n";
+	}
+	void ShowFavouriteFood() {
+		cout << "Puma's main diet is ungulates such as deer, horses, elk, cattle and sheep.";
+	}
+	void ShowHuntingType() {
+		cout << "Puma's hunting type is the following: Waiting his booty in the bush and when she is distracted jumps and drives nails and teeth into it.";
 	}
 };
 
